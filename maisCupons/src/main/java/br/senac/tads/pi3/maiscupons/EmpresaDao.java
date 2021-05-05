8752f52b33b41c3dab0b5f88ed14ec7d7f8f3d11
Bianca <Bianca@192.168.56.1>
2021-05-05 16:53:55 -0300

    Indentação de codigo.

--- maisCupons/src/main/java/br/senac/tads/pi3/maiscupons/EmpresaDao.java
+++ maisCupons/src/main/java/br/senac/tads/pi3/maiscupons/EmpresaDao.java
@@ -10,40 +10,38 @@ import java.sql.SQLException;
  * @author Bianca
  */
 public class EmpresaDao {
-    
+
     private final ConnectionUtils connectionUtils = new ConnectionUtils();
-    
+
     public int cadastrarEmpresa(Empresa empresa) throws ClassNotFoundException, SQLException {
-        String INSERT_USERS_SQL = "INSERT INTO cadastro_empresa" +
-            "  (nome, cnpj, telefone, email, senha) VALUES " +
-            " (?, ?, ?, ?, ?);";
+        String INSERT_USERS_SQL = "INSERT INTO cadastro_empresa"
+                + "  (nome, cnpj, telefone, email, senha) VALUES "
+                + " (?, ?, ?, ?, ?);";
 
         int resultado = 0;
 
         Class.forName("com.mysql.jdbc.Driver");
 
-        try (Connection connection = connectionUtils.obterConexaoBD();
-
-            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
+        try ( Connection connection = connectionUtils.obterConexaoBD();  PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
             preparedStatement.setString(1, empresa.getNome());
             preparedStatement.setString(2, empresa.getCnpj());
             preparedStatement.setString(3, empresa.getTelefone());
             preparedStatement.setString(4, empresa.getEmail());
             preparedStatement.setString(5, empresa.getSenha());
-            
+
             System.out.println(preparedStatement);
-            
+
             resultado = preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
-           
+
             printSQLException(e);
         }
         return resultado;
     }
 
     private void printSQLException(SQLException ex) {
-        for (Throwable e: ex) {
+        for (Throwable e : ex) {
             if (e instanceof SQLException) {
                 e.printStackTrace(System.err);
                 System.err.println("SQLState: " + ((SQLException) e).getSQLState());
