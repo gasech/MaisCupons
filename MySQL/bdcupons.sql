96317b3ad9d314cb7fe5444021ae2d78fd26114a
gsech <gsech@Gab>
2021-05-21 21:23:18 -0300

    Atualização no Script do SQL.

--- MySQL/bdcupons.sql
+++ MySQL/bdcupons.sql
@@ -2,53 +2,54 @@ create database bdcupons;
 
 use bdcupons;
 
+DROP TABLE cadastro_usuario;
+DROP TABLE cadastro_empresa;
+DROP TABLE cadastro_cupom;
+DROP TABLE solicitacao_usuario;
+DROP TABLE solicitacao_empresa;
+
+-- Tabela dos Usuários
 CREATE TABLE cadastro_usuario (
   id int not null PRIMARY KEY auto_increment,
   nome varchar(30) NOT NULL,
   cpf bigint NOT NULL,
   telefone int NOT NULL,
   email varchar(100) NOT NULL,
-  senha varchar(40) NOT NULL
-  );
-  
-  CREATE TABLE cadastro_empresa (  
+  senha varchar(40) NOT NULL,
+  saldo int not null,
+);
+
+-- Tabela das Empresas
+CREATE TABLE cadastro_empresa (  
   id int not null PRIMARY KEY auto_increment,
   nome varchar(30) NOT NULL,
   cnpj bigint NOT NULL ,
   telefone bigint NOT NULL,
   email varchar(100) NOT NULL,
   senha varchar(40) NOT NULL
-  );
-  
-  CREATE TABLE cadastro_cupom (
+);
+
+-- Tabela dos Cupons
+CREATE TABLE cadastro_cupom (
   id int not null PRIMARY KEY auto_increment,
   nome varchar(30) NOT NULL,
   departamento varchar(100) NOT NULL,
   desconto bigint NOT NULL  
-  );
+);
   
-  CREATE TABLE carteira_cashback (
-  id int not null PRIMARY KEY auto_increment,
-  cpf bigint NOT NULL,
-  fk_id_usuario int NOT NULL,
-  fk_id_empresa int NOT NULL,  
-  foreign key (fk_id_usuario) references cadastro_usuario (id),
-  foreign key (fk_id_empresa) references cadastro_empresa (id)
-  );
-  
-  CREATE TABLE solicitacao_usuario(  
+-- Tabela de Solicitações dos Usuários
+CREATE TABLE solicitacao_usuario(  
   id int not null PRIMARY KEY auto_increment,
   nome varchar(30) NOT NULL,
   cpf bigint NOT NULL,
   mensagem varchar(100) NOT NULL 
-  );
-  
-  CREATE TABLE solicitacao_empresa(  
+);
+
+-- Tabela de Solicitações das Empresas
+CREATE TABLE solicitacao_empresa(  
   id int not null PRIMARY KEY auto_increment,
   nome varchar(30) NOT NULL,
   cnpj bigint NOT NULL,
   mensagem varchar(100) NOT NULL 
-  )
-  
-  select  * from carteira_cashback
+);
   
